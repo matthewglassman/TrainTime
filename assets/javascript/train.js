c2fdc3a937b7c777fe1b97f05a480f1d7c6afbb5
matthewglassman <matthewglassmn78@gmail.com>
2017-01-09 16:40:13 -0500

    Added some Javascript code (variables) . Looking at moment.js

---
+++ assets/javascript/train.js
@@ -0,0 +1,33 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDf_NpzVWCr-97gLOUBA4n0k9yJvLR7nbY",
+    authDomain: "train-schedule-c9312.firebaseapp.com",
+    databaseURL: "https://train-schedule-c9312.firebaseio.com",
+    storageBucket: "train-schedule-c9312.appspot.com",
+    messagingSenderId: "2386276282"
+  };
+  firebase.initializeApp(config);
+
+//create a variable to reference the database
+var database = firebase.database();
+
+//Initial Variable Declaration
+var trainName = "";
+var trainDestination = "";
+var trainFrequency = 0;
+var firstTrain = 0;
+var nextTrain = 0;
+var minutesAway = 0;
+var currentTime = moment().format('LT');
+
+console.log(currentTime);
+
+//Capture Button Click
+$("#trainadd").on("click", function(){
+	
+	//Associating the variable to the input of the form field and trimming out unusual spacing
+	trainName = $("#trainname-input").val().trim();
+	trainDestination = $("#destination-input").val().trim();
+	trainFrequency = $("#frequency-input").val().trim();
+	firstTrain = $("#firsttrain-input").val().trim();
+})
