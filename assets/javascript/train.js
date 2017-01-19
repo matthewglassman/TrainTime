21fd61418c8699e33de975da98d8682b5c253b7c
matthewglassman <matthewglassmn78@gmail.com>
2017-01-19 12:37:36 -0500

    Adding a bit more functionality with onClick and reworking time formats

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -22,58 +22,97 @@ var currentTime = moment().format('HH:mm');
 
 console.log(currentTime);
 
-
-//Capture Button Click
-$("#trainadd").on("click", function(){
+$("#trainadd").on("click", function(event){
+	event.preventDefault();
 	alert("You clicked me!");
-	
-	//Associating the variable to the input of the form field and trimming out unusual spacing
+
 	trainName = $("#trainname-input").val().trim();
 	trainDestination = $("#destination-input").val().trim();
 	trainFrequency = $("#frequency-input").val().trim();
 	firstTrain = $("#firsttrain-input").val().trim();
 
-	//Perform calculations with Moment Here.  Make new variables global.
-//---------------------------------------------------------------------------------------------------------------------------
+	console.log (trainName);
+	console.log (trainDestination);
+	console.log (firstTrain);
+	console.log (trainFrequency);
+
 	firstTrain = moment(firstTrain,'hh:mm');
 	nextTrain = moment(nextTrain, 'hh:mm');
 	var computeTrainTimes;
 	console.log(firstTrain);
 	console.log(nextTrain);
 
-	if (firstTrain.isBefore(currentTime)){
+	// if (firstTrain.isBefore(currentTime)){
 
-		while(firstTrain.isBefore(currentTime)){
-			computeTrainTimes = moment(firstTrain).add(trainFrequency,'minutes');
-		}
-		nextTrain = moment(computeTrainTimes).diff(currentTime, 'minutes');
-		console.log(nextTrain);
-	}else{
-		nextTrain = firstTrain;
-		minutesAway = currentTime.to(nextTrain);
-		console.log(minutesAway);
-	};
+	// 	while(firstTrain.isBefore(currentTime)){
+	// 		computeTrainTimes = moment(firstTrain).add(trainFrequency,'minutes');
+	// 	}
+	// 	nextTrain = moment(computeTrainTimes).diff(currentTime, 'minutes');
+	// 	console.log(nextTrain);
+	// }else{
+	// 	nextTrain = firstTrain;
+	// 	minutesAway = currentTime.to(nextTrain);
+	// 	console.log(minutesAway);
+	// };
+});
+
+// //Capture Button Click
+// $("#trainadd").on("click", function(){
+// 	alert("You clicked me!");
+	
+// 	//Associating the variable to the input of the form field and trimming out unusual spacing
+// 	trainName = $("#trainname-input").val().trim();
+// 	trainDestination = $("#destination-input").val().trim();
+// 	trainFrequency = $("#frequency-input").val().trim();
+// 	firstTrain = $("#firsttrain-input").val().trim();
+
+// 	console.log(trainName);
+// 	console.log(trainDestination);
+// 	console.log(trainFrequency);
+// 	console.log(firstTrain);
+// });
+
+	//Perform calculations with Moment Here.  Make new variables global.
+//---------------------------------------------------------------------------------------------------------------------------
+	// firstTrain = moment(firstTrain,'hh:mm');
+	// nextTrain = moment(nextTrain, 'hh:mm');
+	// var computeTrainTimes;
+	// console.log(firstTrain);
+	// console.log(nextTrain);
+
+	// if (firstTrain.isBefore(currentTime)){
+
+	// 	while(firstTrain.isBefore(currentTime)){
+	// 		computeTrainTimes = moment(firstTrain).add(trainFrequency,'minutes');
+	// 	}
+	// 	nextTrain = moment(computeTrainTimes).diff(currentTime, 'minutes');
+	// 	console.log(nextTrain);
+	// }else{
+	// 	nextTrain = firstTrain;
+	// 	minutesAway = currentTime.to(nextTrain);
+	// 	console.log(minutesAway);
+	// };
 
 //---------------------------------------------------------------------------------------------------------------------------
 
 //.ref and .push the items to database.
-database.ref().push({
-	'Train': trainName,
-	'Destination': trainDestination,
-	'Frequency': trainFrequency,
-	'Next Train': nextTrain, //From Calculations
-	'Minutes Away': minutesAway
-});
-return false;
-});
+// database.ref().push({
+// 	'Train': trainName,
+// 	'Destination': trainDestination,
+// 	'Frequency': trainFrequency,
+// 	'Next Train': nextTrain, //From Calculations
+// 	'Minutes Away': minutesAway
+// });
+// // return false;
+// // };
 
-//create event listener to take snapshot from ref and append to the train table.
-//checking database for data and creating snapshot
-database.ref().on("child_added", function(childSnapshot) {
+// //create event listener to take snapshot from ref and append to the train table.
+// //checking database for data and creating snapshot
+// database.ref().on("child_added", function(childSnapshot) {
 
 
-$("#train_table").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td></tr>");
+// $("#train_table").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td></tr>");
 
-}, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-});
+// }, function(errorObject) {
+//       console.log("Errors handled: " + errorObject.code);
+// });
